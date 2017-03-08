7cdd526058d3bb458c828025a08fab1e04bbf815
fqiyou <yc@fqiyou.com>
2017-03-08 21:45:52 +0800

    YangChao

--- instabce_db/single/auto_install.sh
+++
@@ -1,9 +0,0 @@
-###############################################
-# File Name:auto_install.sh
-# Author: Yc
-# Mail : 1522105005@qq.com
-# Created Time: Sat 24 Sep 2016 05:30:41 PM CST
-#===============================================
-#!/bin/bash
-
-echo "lalala"
